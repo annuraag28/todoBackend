6d5a802bbc41dab7adf5381fd7de35200a10faea
Anurag Kumar1 <Anurag.Kumar1@XEBIAINDIA.LOCAL>
2023-08-22 16:24:57 +0530

    user login is modified

--- controllers/user.js
+++ controllers/user.js
@@ -1,25 +1,24 @@
 import { User } from "../models/user.js";
 import bcrypt from "bcrypt";
 import { sendCookie } from "../utils/features.js";
-import jwt from "jsonwebtoken";
 import ErrorHandler from "../middlewares/error.js";
 
 
 //using try catch block wherever the code is async.
 
-export const login = async (req, res)=>{
+export const login = async (req,res, res)=>{
     try {
         const {email,password} = req.body;
 
         //password mein select is false, that is why we have to take the password exclusively
-        let user = await User.findOne({email}).select("+password");
+        const user = await User.findOne({email}).select("+password");
         // if(!user){
         //     res.status(404).json({
         //         success:false,
         //         message:"Invalid email or password",
         //     })
         // }
-        if(!user) return next(new ErrorHandler("Invalid email or password",404));
+        if(!user) return next(new ErrorHandler("Invalid email or password",400));
 
         const isMatch = await bcrypt.compare(password,user.password);
         // if(!isMatch){
@@ -28,7 +27,8 @@ export const login = async (req, res)=>{
         //         message:"Invalid email or password",
         //     })
         // }
-        if(!isMatch) return next(new ErrorHandler("Invalid email or password",404));
+        if(!isMatch) 
+        return next(new ErrorHandler("Invalid email or password",400));
 
         sendCookie(user,res,`Welcome back ${user.name}`,200);
     } catch (error) {
@@ -46,7 +46,7 @@ export const register = async (req, res)=>{
     //         message:"User is already registered",
     //     })
     // }
-    if(user) return next(new ErrorHandler("User Already Exists",404));
+    if(user) return next(new ErrorHandler("User Already Exists",400));
     const hashedPassword = await bcrypt.hash(password,10);
     user = await User.create({name, email, password:hashedPassword});
     //to pass cookie we will generate one token using jwt
@@ -67,22 +67,34 @@ export const getMyProfile = (req,res)=>{
         // console.log(decoded); //{ _id: '64cdef4f7b78b1e9c55f9e78', iat: 1691222313 }
 };
 
-
-export const logout = async (req, res)=>{
-    try {
-        res.status(200).cookie("token","",{
-            expires:new Date(Date.now()),
-            sameSite: process.env.NODE_ENV==="Developement" ? "lax":"none",
-            secure: process.env.NODE_ENV==="Developement" ? false:true, 
-        })
-        .json({
-            success:true,
-            message:"Logged out successfully"
-        });
-    } catch (error) {
-        next(error);
-    }
-}
+export const logout = (req, res) => {
+    res
+      .status(200)
+      .cookie("token", "", {
+        expires: new Date(Date.now()),
+        sameSite: process.env.NODE_ENV === "Develpoment" ? "lax" : "none",
+        secure: process.env.NODE_ENV === "Develpoment" ? false : true,
+      })
+      .json({
+        success: true,
+        user: req.user,
+      });
+  };
+// export const logout = (req, res)=>{
+//     try {
+//         res.status(200).cookie("token","",{
+//             expires:new Date(Date.now()),
+//             sameSite: process.env.NODE_ENV==="Developement" ? "lax":"none",
+//             secure: process.env.NODE_ENV==="Developement" ? false:true, 
+//         })
+//         .json({
+//             success:true,
+//             user:req.user
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// }
 
 
 
